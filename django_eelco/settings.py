e43e2cf0c9b33e809c27445285973abbbfeba17d
Eelco Wesemann <sgaduuw@users.noreply.github.com>
2020-03-25 13:22:14 +0100

    Disable STATICFILES_STORAGE

--- django_eelco/settings.py
+++ django_eelco/settings.py
@@ -133,4 +133,4 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

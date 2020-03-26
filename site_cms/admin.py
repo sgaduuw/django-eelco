100b401409511079f41a3bc5d9c0edf1817a6a0d
Eelco Wesemann <eelco.wesemann@gmail.com>
2020-03-26 23:08:36 +0100

    Register Sitedomain model with admin

--- site_cms/admin.py
+++ site_cms/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Site, Author, Tag, Category, Attachment, Content, ContentMeta
+from .models import Site, Sitedomain, Author, Tag, Category, Attachment, Content, ContentMeta
 
 
 # Register your models here.
 admin.site.register(Site)
+admin.site.register(Sitedomain)
 admin.site.register(Author)
 admin.site.register(Tag)
 admin.site.register(Category)

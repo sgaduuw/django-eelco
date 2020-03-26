afdc058fc25c49a22d366ee94d015eacbb308546
Eelco Wesemann <eelco.wesemann@gmail.com>
2020-03-26 17:14:24 +0100

    Register Models in Admin

--- site_cms/admin.py
+++ site_cms/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Site, Author, Tag, Category, Attachment, Content, ContentMeta
+
 
 # Register your models here.
+admin.site.register(Site)
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Attachment)
+admin.site.register(Content)
+admin.site.register(ContentMeta)

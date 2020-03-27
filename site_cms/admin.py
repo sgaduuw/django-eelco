c4d9d5a3139d612d4a435948efdf3c8c0c0c584a
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-03-27 22:21:08 +0100

    Adjust listing of categories

--- site_cms/admin.py
+++ site_cms/admin.py
@@ -4,11 +4,14 @@ from .models import Siteinfo, Author, Tag, Category, Attachment, Content, Conten
 class TagDisplay(admin.ModelAdmin):
     list_display = ('name', 'description')
 
+class CategoryDisplay(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
 # Register your models here.
 admin.site.register(Siteinfo)
 admin.site.register(Author)
 admin.site.register(Tag, TagDisplay)
-admin.site.register(Category)
+admin.site.register(Category, CategoryDisplay)
 admin.site.register(Attachment)
 admin.site.register(Content)
 admin.site.register(ContentMeta)

f7b503194ad3e927b4f4f281edb7bf061a46c18d
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-03-27 22:17:51 +0100

    Adjust listing of tags

--- site_cms/admin.py
+++ site_cms/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Siteinfo, Author, Tag, Category, Attachment, Content, ContentMeta
 
+class TagDisplay(admin.ModelAdmin):
+    list_display = ('name', 'description')
 
 # Register your models here.
 admin.site.register(Siteinfo)
 admin.site.register(Author)
-admin.site.register(Tag)
+admin.site.register(Tag, TagDisplay)
 admin.site.register(Category)
 admin.site.register(Attachment)
 admin.site.register(Content)

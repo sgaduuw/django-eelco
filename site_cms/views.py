9e6aa0f242b86af27e7c713fd063ebb901641c86
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-03-27 23:32:08 +0100

    Add a placeholder view, template and urlpattern for the index page, just to display something from the database

--- site_cms/views.py
+++ site_cms/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from site_cms.models import Content, Author, ContentMeta
+
+def index(request):
+    """ Front page placeholder """
+    num_blog_posts = Content.objects.count()
+    post_list = Content.objects.all()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_blog_posts': num_blog_posts,
+        'num_authors': num_authors,
+        'post_list': post_list,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)

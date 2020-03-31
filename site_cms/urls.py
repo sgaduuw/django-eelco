77199560340d493dc97bbb9f93ed9ff255ffd214
Eelco Wesemann <eelco.wesemann@gmail.com>
2020-03-31 21:50:08 +0200

    Route slug-based url to BlogPostView

--- site_cms/urls.py
+++ site_cms/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<slug:slug>/', views.BlogPostView.as_view(), name='page'),
+    #path('<int:year>/', views.page, name='page'),
+    #re_path(r'^(?P<year>[0-9]{4})/<slug:content.slug>/$', views.page, name='page'),
 ]

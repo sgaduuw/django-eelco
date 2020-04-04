0a5387affc595f91839a11fdc0affe8c752a8cc2
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-04-04 15:32:57 +0200

    Use the slug of author, not str

--- site_cms/urls.py
+++ site_cms/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('author/<str:author>', views.author, name='author'),
+    path('author/<slug:author>', views.author, name='author'),
     path('<int:year>/<slug:slug>/', views.blog, name='blog'),
     path('<slug:slug>/', views.page, name='page'),
     #path('<int:year>/', views.page, name='page'),

ccb3017e4b6383b54097c5a0d675f81adbf16343
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-04-05 11:09:24 +0200

    Cleanup

--- site_cms/urls.py
+++ site_cms/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('author/<slug:author>', views.author, name='author'),
     path('<int:year>/<slug:slug>/', views.blog, name='blog'),
     path('<slug:slug>/', views.page, name='page'),
-    #path('<int:year>/', views.page, name='page'),
-    #re_path(r'^(?P<year>[0-9]{4})/<slug:content.slug>/$', views.page, name='page'),
 ]

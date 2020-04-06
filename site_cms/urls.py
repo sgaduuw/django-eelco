4aa9f1759e7a4b286b3ecb655f24b89c156298c2
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-04-06 22:35:22 +0200

    Plug paths for tag based urls

--- site_cms/urls.py
+++ site_cms/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('tags/', views.tag_list, name='tag_list'),
+    path('tag/<slug:tag>', views.tag_detail, name='tag_detail'),
+
     path('author/<slug:author>', views.author, name='author'),
     path('<int:year>/<slug:slug>/', views.blog, name='blog'),
     path('<slug:slug>/', views.page, name='page'),

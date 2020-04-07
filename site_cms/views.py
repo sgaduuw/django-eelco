37b423a7b8f5bbfc610e7db35f25d19d19988251
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-04-07 23:12:17 +0200

    Placeholder views for tag page and tags listing

--- site_cms/views.py
+++ site_cms/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-from site_cms.models import Content, Author
+from site_cms.models import Content, Author, Tag, Category
 
 def index(request):
-    """ Front page placeholder """
+    """ Front page listing """
     blog_post_list = Content.objects.filter(ctype='1', contentstatus='2')
     page_post_list = Content.objects.filter(ctype='2', contentstatus='2')
     content = Content.objects.all()
@@ -19,6 +19,7 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 def author(request, author):
+    """ Author page """
     author = Content.objects.filter(author__name__iexact=author)
     context = {
         'author': author,
@@ -26,14 +27,14 @@ def author(request, author):
     return render(request, 'author.html', context=context)
 
 def page(request, slug):
-
+    """ 'static' page """
     context = {
         'slug': slug,
     }
     return render(request, 'page.html', context=context)
 
 def blog(request, year, slug):
-
+    """ Blog page """
     content = Content.objects.get(slug=slug, publishdate__year=str(year))
 
     context = {
@@ -43,3 +44,25 @@ def blog(request, year, slug):
     }
 
     return render(request, 'blog.html', context=context)
+
+def tag_list(request):
+    """ Tag listing """
+    tags = Tag.objects.all()
+    
+    context = {
+        'tags': tags,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'tags.html', context=context)
+
+def tag_detail(request, tag):
+    """ Single tag page """
+    tag = Tag.objects.filter(name=tag)
+
+    context = {
+        'tag': tag,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'tag_detail.html', context=context)

77f6faa235a766d92aa4d5db65bc761102a3b513
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-04-07 23:11:22 +0200

    Fix match of path

--- site_cms/urls.py
+++ site_cms/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('tags/', views.tag_list, name='tag_list'),
-    path('tag/<slug:tag>', views.tag_detail, name='tag_detail'),
+    path('tag/<slug:tag>/', views.tag_detail, name='tag_detail'),
 
     path('author/<slug:author>', views.author, name='author'),
     path('<int:year>/<slug:slug>/', views.blog, name='blog'),

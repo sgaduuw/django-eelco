d3b82dfd407fbe562a03b23df7db16bfe556c750
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-04-11 23:09:08 +0200

    Use more descriptive variable name

--- site_cms/urls.py
+++ site_cms/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     path('tags/', views.taxonomy_list, { 'taxonomy_type': 'tags' }),
-    path('tag/<slug:taxonomy>/', views.taxonomy_detail, { 'taxonomy_type': 'tag' }),
+    path('tag/<slug:taxonomy_name>/', views.taxonomy_detail, { 'taxonomy_type': 'tag' }),
 
     path('categories/', views.taxonomy_list, { 'taxonomy_type': 'categories' }),
-    path('category/<slug:taxonomy>/', views.taxonomy_detail, { 'taxonomy_type': 'category' }),
+    path('category/<slug:taxonomy_name>/', views.taxonomy_detail, { 'taxonomy_type': 'category' }),
 
     path('author/<slug:author>', views.author, name='author'),
     path('<int:year>/<slug:slug>/', views.blog, name='blog'),

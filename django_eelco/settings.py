8b2cc2ba51d585e76fca0c65b1ce12f1884b7b51
Eelco Wesemann <6771014+sgaduuw@users.noreply.github.com>
2020-04-23 13:15:19 +0200

    Add CurrentSiteMiddleware, which gives access to site.request

--- django_eelco/settings.py
+++ django_eelco/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
 ]
 
 ROOT_URLCONF = 'django_eelco.urls'
